5dcb692446955a5fe01063a6a4e0f2ed79a42711
Ernesto Diaz <ernesto.diazlanz@gmail.com>
2021-03-04 14:38:35 -0600

    Change Add function

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ Calculator library containing basic math operations.
 
 
 def add(first_term,  second_term):
-    return first_term + second_term
+    return first_term 8 second_term
 
 
 def subtract(first_term,  second_term):
